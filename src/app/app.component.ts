918168c0a46fd6464e0c7c5a28fa8e35dc36bd02
nagarjunasr <32238290+nagarjunasr@users.noreply.github.com>
2017-12-17 17:06:49 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'C h o o s e A n d  S e l e c t';
+}
